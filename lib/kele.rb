86fa28c9980ceec94834a1711171af7e5291ef98
realtimschmidt <schmidt.tim.j@gmail.com>
2017-06-09 16:14:44 -0400

    Mentor Availability Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,13 +12,17 @@ class Kele
       @auth_token = response["auth_token"]
       puts "#{email} has sucessfully logged in"
     else
-      puts "Login invalid"
+      puts "Login invalid, please try again."
     end
   end
 
-
   def get_me
     response = Kele.get("/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = Kele.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 end
