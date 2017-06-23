2a5d3b611483746c9a64723803d484e2c905b920
realtimschmidt <schmidt.tim.j@gmail.com>
2017-06-23 08:46:18 -0400

    removed parse

--- lib/kele.rb
+++ lib/kele.rb
@@ -43,6 +43,5 @@ class Kele
 
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
     response = Kele.post("/checkpoint_submissions", body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment }, headers: { "authorization" => @auth_token })
-    JSON.parse(response.body)
   end
 end
